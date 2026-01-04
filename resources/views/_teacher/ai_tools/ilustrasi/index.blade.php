fc713688547253f855c7f5acf901ba81fd06d7aa
HanNajib <arroyhan27@gmail.com>
2026-01-04 11:28:33 +0700

    feat: Update links for generating illustrations and materials in AI tools

--- resources/views/_teacher/ai_tools/ilustrasi/index.blade.php
+++ resources/views/_teacher/ai_tools/ilustrasi/index.blade.php
@@ -17,7 +17,7 @@
             <div class="inline-flex gap-x-2">
                 <a navigate
                     class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none font-bolder"
-                    href="{{ route('admin.students.add') }}">
+                    href="{{ route('teacher.ai.ilustrasi.add') }}">
                     @include('_admin._layout.icons.add')
                     Generate Ilustrasi
                 </a>
--- resources/views/_teacher/ai_tools/materi/index.blade.php
+++ resources/views/_teacher/ai_tools/materi/index.blade.php
@@ -17,7 +17,7 @@
             <div class="inline-flex gap-x-2">
                 <a navigate
                     class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none font-bolder"
-                    href="{{ route('admin.students.add') }}">
+                    href="{{ route('teacher.ai.materi_ajar.add') }}">
                     @include('_admin._layout.icons.add')
                     Generate Materi
                 </a>
